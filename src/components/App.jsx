13e8c54ecbf25d03c5045a05a395a3662fadba95
Guess <contact@andrewjonhardt.com>
2024-12-27 18:19:51 -0600

    tracking and updating text in textarea appears to be easy.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,11 @@ import '../styling/index.css'
 function App() {
   const [fileText, setFileText] = useState('Test text');
 
+
   return (
     <>
       {/*fileText !== '' && <textarea value={fileText}></textarea>*/}
-      <textarea value={fileText}></textarea>
+      <textarea value={fileText} onChange={e=>setFileText(e.target.value)}></textarea>
     </>
   )
 }
