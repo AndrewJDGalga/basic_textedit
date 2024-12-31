c71927dfa319e4891f0f15ee577d1049250945f6
Guess <contact@andrewjonhardt.com>
2024-12-31 14:41:58 -0600

    updated spacing because my project.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,20 @@ import CustButton from './CustButton';
 import '../styling/index.css'
 
 function App() {
-  const [fileText, setFileText] = useState('');
+    const [filePath, setFilePath] = useState('');
+    const [fileText, setFileText] = useState('');
 
+    return (
+        <>
+            <section>
+                <CustButton id="save" inner="&#128190;" func={() => { }}></CustButton>
+                <CustButton id="reset" inner="&#10227;" func={() => { }}></CustButton>
+                <CustButton id="load" inner="&#10581;" func={() => { }}></CustButton>
+            </section>
+            <textarea value={fileText} onChange={e => setFileText(e.target.value)} placeholder='Click inside this box and start typing. OR, select &#10581; in the left-side menu to load a .txt file...'></textarea>
 
-  return (
-    <>
-      <section>
-        <CustButton id="save" inner="&#128190;" func={()=>{}}></CustButton>
-        <CustButton id="reset" inner="&#10227;" func={()=>{}}></CustButton>
-        <CustButton id="load" inner="&#10581;" func={()=>{}}></CustButton>
-      </section>
-      <textarea value={fileText} onChange={e=>setFileText(e.target.value)} placeholder='Click inside this box and start typing. OR, select &#10581; in the left-side menu to load a .txt file...'></textarea>
-      
-    </>
-  )
+        </>
+    )
 }
 
 export default App
